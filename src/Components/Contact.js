266b4e1029e73183f91885e40dd8d4928f6406d0
swethak44 <swethakammari44@gmail.com>
2024-11-27 11:41:19 +0530

    init

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './contact.css';
+import { Container,Row,Col } from 'react-bootstrap';
 
 export default function Contact() {
   return (
@@ -12,6 +13,27 @@ export default function Contact() {
                     here to help you with risk analysis, investment planning, and achieving your financial goals.
                 </p>
             </div> 
+        </div>
+
+        <div className='bgrnd'>
+          <div className="container">
+            <Row>
+              <Col lg={10}>
+              <div className='tbox'>
+                <h3 className='mt-5 p-5'> Get in Touch</h3>
+                <p>
+                  Need advice or have questions? Contact us to schedule a discovery session with Fincart's Financial Planning Services. 
+                  We'll assess your financial situation and create a plan just for you
+                </p>
+              </div>
+
+              <div className='fbox'>
+
+                </div>
+                 
+              </Col>
+            </Row>
+          </div>
         </div>
         
     </div>
